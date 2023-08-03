19352f8141f5fe6c48fc5101de9a57532892c44d
J Venters <jventers@protonmail.com>
2023-08-03 19:59:39 +0100

    corrected class name in super call

--- models/pointnet2.py
+++ models/pointnet2.py
@@ -5,7 +5,7 @@ from .pointnet2_utils import PointNetSetAbstraction
 
 class PointNet2(nn.Module):
     def __init__(self, num_class, normal_channel=True):
-        super(get_model, self).__init__()
+        super(PointNet2, self).__init__()
         in_channel = 6 if normal_channel else 3
         self.normal_channel = normal_channel
         self.sa1 = PointNetSetAbstraction(
